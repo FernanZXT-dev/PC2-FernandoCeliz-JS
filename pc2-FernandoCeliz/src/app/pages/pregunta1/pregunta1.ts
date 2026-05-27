821ff6b45bf6ebda7c0d740986b33247d460fc32
Fernando <celizfernando82@gmail.com>
2026-05-27 04:20:52 +0000

    Creacion del proyecto Base Final

---
+++ pc2-FernandoCeliz/src/app/pages/pregunta1/pregunta1.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pregunta1',
+  imports: [],
+  templateUrl: './pregunta1.html',
+  styleUrl: './pregunta1.css',
+})
+export class Pregunta1 {}
---
+++ pc2-FernandoCeliz/src/app/pages/pregunta2/pregunta2.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pregunta2',
+  imports: [],
+  templateUrl: './pregunta2.html',
+  styleUrl: './pregunta2.css',
+})
+export class Pregunta2 {}
