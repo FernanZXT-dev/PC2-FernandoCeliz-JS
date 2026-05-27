890159db26c0b9d7ee9d5f07cb07346ee27e99fe
Fernando <celizfernando82@gmail.com>
2026-05-27 03:57:29 +0000

    Creacion del proyecto Base Final

--- pc2-FernandoCeliz/src/app/app.routes.ts
+++ pc2-FernandoCeliz/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { Inicio } from './pages/inicio/inicio';
+import { Producto } from './pages/producto/producto';
+import { Registro } from './pages/registro/registro';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'inicio', pathMatch: 'full' },
+  { path: 'inicio', component: Inicio },
+  { path: 'productos', component: Producto },
+  { path: 'registro', component: Registro },
+  { path: '**', redirectTo: 'inicio' }
+];
