844c9d9bb20e97daf2465ebfa5847d2115038e6b
Fernando <celizfernando82@gmail.com>
2026-05-28 16:42:47 +0000

    Creación del proyecto Angular FIna pc2

--- pc2-FernandoCeliz/src/app/pages/pregunta1/pregunta1.ts
+++ pc2-FernandoCeliz/src/app/pages/pregunta1/pregunta1.ts
@@ -1,10 +1,63 @@
 import { Component } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { DecimalPipe } from '@angular/common';
 @Component({
   selector: 'app-pregunta1',
   standalone: true,
-  imports: [],
+  imports: [FormsModule,DecimalPipe],
   templateUrl: './pregunta1.html',
   styleUrl: './pregunta1.css',
 })
-export class Pregunta1 {}
+export class Pregunta1 {
+  n1: number = 0;
+  n2: number = 0;
+  n3: number = 0;
+  n4: number = 0;
+
+  suma: number = 0;
+  promedio: number = 0;
+  estado: string = '';
+  mensaje: string = '';
+
+  procesar(): void {
+    if (!this.validarNotas()) {
+      this.mensaje = 'Todas las notas deben estar entre 0 y 20.';
+      this.limpiarResultados();
+      return;
+    }
+    this.suma = this.n1 + this.n2 + this.n3 + this.n4;
+    this.promedio = this.suma / 4;
+
+    if (this.promedio >= 17) {
+      this.estado = 'Excelente';
+    } else if (this.promedio >= 13) {
+      this.estado = 'Regular';
+    } else {
+      this.estado = 'Riesgo';
+    }
+
+    this.mensaje = 'Proceso realizado correctamente.';
+  }
+
+  validarNotas(): boolean {
+    return this.n1 >= 0 && this.n1 <= 20 &&
+           this.n2 >= 0 && this.n2 <= 20 &&
+           this.n3 >= 0 && this.n3 <= 20 &&
+           this.n4 >= 0 && this.n4 <= 20;
+  }
+
+  limpiarResultados(): void {
+    this.suma = 0;
+    this.promedio = 0;
+    this.estado = '';
+  }
+
+  limpiarFormulario(): void {
+    this.n1 = 0;
+    this.n2 = 0;
+    this.n3 = 0;
+    this.n4 = 0;
+    this.mensaje = '';
+    this.limpiarResultados();
+  }
+}
--- pc2-FernandoCeliz/src/app/pages/pregunta2/pregunta2.ts
+++ pc2-FernandoCeliz/src/app/pages/pregunta2/pregunta2.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+interface Persona {
+  nombre: string;
+  correo: string;
+  peso: number;
+  altura: number;
+  imc: number;
+  estado: string;
+}
 @Component({
   selector: 'app-pregunta2',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './pregunta2.html',
   styleUrl: './pregunta2.css',
 })
-export class Pregunta2 {}
+export class Pregunta2 {
+  nuevoPersona: Persona = { nombre: '', correo:'', peso: 0, altura: 0, imc:0, estado:'' };
+  listaPersona: Persona[] = [];
+    agregar() {
+
+    if (this.nuevoPersona.nombre.trim() !== '') {
+      this.listaPersona.push({ ...this.nuevoPersona });
+      this.limpiar();
+    }
+    
+  }
+    limpiar() {
+    this.nuevoPersona = { nombre: '', correo: '', peso: 0, altura:0, imc:0, estado:'' };
+  }
+
+}
